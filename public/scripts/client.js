73e81bac66d4386be9802a316db42af63e3ac147
Clement Shum <clement.shum128@gmail.com>
2023-03-24 20:58:41 -0400

    Add a Second Toggle Button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,95 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  // function to fetch tweets from /tweets page
+
+  const loadtweets = function () {
+    return $.ajax({
+      type: "GET",
+      url: "http://localhost:8080/tweets",
+      success: function (result) {
+        renderTweets(result);
+      },
+    });
+  };
+
+  const renderTweets = function (tweets) {
+    // loop through object and append createTweetElemnt into .container
+    for (const tweet of tweets) {
+      $("#tweets-container").prepend(createTweetElement(tweet));
+    }
+  };
+
+  const customEscape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const createTweetElement = (tweetObj) => {
+    return `
+      <article class="tweet-layout">
+        <header class="flex-row-spaced">
+          <span class="tweet-icon flex-row-spaced">
+            <img src="${tweetObj.user.avatars}" />
+            <p>${tweetObj.user.name}</p>
+          </span>
+          <p>${tweetObj.user.handle}</p>
+        </header>
+        <div>
+          <p class="tweet-content">${customEscape(tweetObj.content.text)}</p>
+        </div>
+        <footer class="tweet-foot flex-row-spaced">
+          <p>${timeago.format(tweetObj.created_at)}</p>
+          <div class="tweet-icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `;
+  };
+
+  loadtweets();
+
+  // Submit form handler
+
+  $("#tweet-form").submit(function (event) {
+    event.preventDefault(); // presents the default behaviour of the form
+    let textLength = $("#tweet-text").val().length;
+    if (textLength < 1) {
+      $("#error-message").slideUp();
+      $("#error-message")
+        .html("&#x26A0 Please enter in some text! &#x26A0")
+        .slideDown();
+    } else if (textLength > 140) {
+      $("#error-message").slideUp();
+      $("#error-message")
+        .html(
+          "&#x26A0 The max amount of text is 140 per tweet! Please remove a few characters! &#x26A0"
+        )
+        .slideDown();
+    } else {
+      //Make a AJAX Call
+      $.ajax({
+        url: "/tweets",
+        type: "POST",
+        data: $(this).serialize(),
+        success: () => {
+          $("#error-message").slideUp();
+          $("#tweet-form")[0].reset();
+          $("#tweets-container").empty();
+          loadtweets();
+        },
+      });
+    }
+  });
+
+  // Toggle the form
+
+  $(".toggle-form-button").click(function () {
+    $(".new-tweet").toggle("slow");
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -10,4 +10,22 @@ $(document).ready(function () {
       counterElement.removeClass("red");
     }
   });
+
+  // Top of page button
+  // Hide the topbutton on page load/ready.
+  $(".top-of-page").hide();
+
+  // Check to see if the window is top if not then display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+      $(".top-of-page").show();
+    } else {
+      $(".top-of-page").hide();
+    }
+  });
+
+  // Goes back to top of page when clicked
+  $(".top-of-page").click(function () {
+    window.scrollTo(0, 0);
+  });
 });
